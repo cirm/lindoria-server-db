ef1e67ed78dbcce7e233b872cf6ca1a080ce03a5
cirm <m.jullinen@gmail.com>
2015-12-30 14:31:27 +0200

    Added organization GET functions

--- src/011.create_GET_request_functions.sql
+++ src/011.create_GET_request_functions.sql
@@ -1,45 +1,35 @@
-select row_to_json(t)
-from (
-select eo.oname, eo.display, eo.owner, ep.display, eo.abbr, eo.treasury
-from empires.organizations eo, empires.persons ep
-where eo.owner = ep.pname) t;
+CREATE OR REPLACE FUNCTION empires.get_organizations()
+RETURNS JSON AS
+$BODY$
+BEGIN
+    RETURN (
+    SELECT array_to_json(array_agg(row_to_json(t.*)))
+    FROM (
+        SELECT eo.oname, eo.display, eo.owner, ep.display, eo.abbr, eo.treasury
+        FROM empires.organizations eo, empires.persons ep
+        WHERE eo.owner = ep.pname) t);
+END;
+$BODY$
+LANGUAGE plpgsql;
 
-select eh.holding_id, eh.level, eh.owner, ep.pname, ep.display, eh.type
-from empires.holdings eh, empires.provinces ep
-where owner = 'lvahtkond' and ep.pname = eh.province;
-
-with holdings as (select eh.holding_id, eh.level, eh.owner, ep.pname, ep.display, eh.type
-                  from empires.holdings eh, empires.provinces ep
-                  where owner = 'lvahtkond' and ep.pname = eh.province)
-
-
-
-select eo.oname, eo.display, eo.owner, ep.display, eo.abbr,(array_to_json(array_agg(select row_to_json(t) from (select holding_id, level, owner, pname, display, type from holdings)t))) as holdings, eo.treasury
-from empires.organizations eo, empires.persons ep, holdings
-where eo.owner = ep.pname and eo.oname = 'lvahtkond' ;
-
-select row_to_json(t)
-from ()t
-
-(select array( select holding_id from holdings)),
-
-
-
------------------------------
-select row_to_json(t) as org_details from (
-select eo.oname, eo.display, eo.owner, eo.abbr, eo.treasury,
-(
-    select array_to_json(array_agg(row_to_json(d)))
-    from (select eh.holding_id, eh.level, ep.pname, ep.display, eh.type
-                            from empires.holdings eh, empires.provinces ep
-                            where owner = 'lvahtkond' and ep.pname = eh.province ) d) as holdings
-                            from empires.organizations eo where eo.oname = 'lvahtkond')t;
+CREATE OR REPLACE FUNCTION empires.get_organization_details(
+    IN i_oname  VARCHAR(10)
+) RETURNS JSON AS
+$BODY$
+BEGIN
+    RETURN (
+    SELECT row_to_json(t.*) as org_details
+    FROM (
+        SELECT eo.oname, eo.display, eo.owner, eo.abbr, eo.treasury, (
+            SELECT array_to_json(array_agg(row_to_json(d)))
+            FROM (
+                SELECT eh.holding_id, eh.level, ep.pname, ep.display, eh.type
+                FROM empires.holdings eh, empires.provinces ep
+                WHERE owner = i_oname AND ep.pname = eh.province) d) AS holdings
+        FROM empires.organizations eo WHERE eo.oname = i_oname)t
 
 
-
----------------------------------
-select row_to_json(t) as org
-from (
-select eo.oname, eo.display, eo.owner, ep.display, eo.abbr, eo.treasury
-from empires.organizations eo, empires.persons ep
-where eo.owner = ep.pname) t;
+     );
+END;
+$BODY$
+LANGUAGE plpgsql;
